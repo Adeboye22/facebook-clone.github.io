5a7de03b1768f067d839f40e72ef32c0cf63b322
Oluwadamilola Oyeniya <oyeniyaoluwadamilola@outlook.com>
2023-03-09 08:15:30 +0100

    facebook clone

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var theme = document.getElementById("theme-btn");
 var body = document.querySelector("body");
 var txt = document.querySelector("p");
-var brand = document.querySelector("brand-b");
+var brand = document.querySelector("strong");
+var span = document.querySelector("span");
 var isBlack = false;
 
 //this function changes the theme of the app
@@ -12,22 +13,26 @@ theme.addEventListener("click", function myBtn() {
         theme.style.background = "black";
         theme.style.color = "white";
         txt.style.color = "black";
-        
+        brand.style.color = "black";
+        span.style.color = "black";
+
         //change the text on the button when clicked
-        theme.onclick = function() {
+        theme.onclick =() => {
             theme.innerHTML = "Enable Dark Mode";
             body.style.transition = "all 1s";
             theme.style.transition = "all 1s";
-            txt.style.transition = "all 1s";
+            txt.style.transition = "all 1s";   
         }
     } else {
         body.style.background = "rgb(23, 33, 36)";
         theme.style.background = "white";
         theme.style.color = "black";
         txt.style.color = "white";
+        brand.style.color = "white";
+        span.style.color = "white";
         
         //change the text on the button when clicked
-        theme.onclick = function() {
+        theme.onclick =() => {
             theme.innerHTML ="Enable Light Mode";
             body.style.transition = "all 1s";
             theme.style.transition = "all 1s";
